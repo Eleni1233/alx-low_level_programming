738350cd54c3f7b079035f37c8390c0b5c0628cc
Eleni1233 <110999112+Eleni1233@users.noreply.github.com>
2022-09-13 13:04:35 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@
 
 int print_last_digit(int n)
 {
-  int l, p;
+	int l, p;
 
-  l = n % 10;
-  if (l <  0)
-    l = -l;
-  p = '0' + l;
-  _putchar(p);
-  return (l);
+	l = n % 10;
+	if (l <  0)
+		l = -l;
+	p = '0' + l;
+	_putchar(p);
+	return (l);
 }
