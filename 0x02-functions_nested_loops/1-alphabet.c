ba9cf0a461e3685e989413d1458fd4d9af9ba164
Eleni1233 <110999112+Eleni1233@users.noreply.github.com>
2022-09-13 12:43:31 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,35 +1,18 @@
 #include "main.h"
 
-
-
 /**
-
  * print_alphabet - entry point
-
  * Description: Prints the alphabet with _putchar
-
  * Return: void
-
  */
 
-
-
 void print_alphabet(void)
-  
 {
-  
-  char c;
-  
+	char c;
 
-  
-  for (c = 'a'; c <= 'z'; c++)
-    
-    {
-      
-      _putchar(c);
-      
-    }
-  
-  _putchar('\n');
-  
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
