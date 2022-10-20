764fc8e54a9e1a92432b47cae6ebc785de27064d
Eleni1233 <110999112+Eleni1233@users.noreply.github.com>
2022-10-20 11:13:40 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,3 +1,12 @@
-100-first.c
-100-first.c
-100-first.c
+#include <stdio.h>
+
+/**
+ * bmain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
