65cf827b9949cd27e5402b449eaae0114732facb
Eleni1233 <110999112+Eleni1233@users.noreply.github.com>
2022-10-25 10:23:13 -0700

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,3 +1,18 @@
-4-free_listint.c
-4-free_listint.c
-4-free_listint.c
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
